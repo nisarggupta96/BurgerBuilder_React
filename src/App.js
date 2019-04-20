32090cbf0b5865c4c20ba493a9a9a4c5787f63b3
Nisarg <nisarg.gupta@moovweb.com>
2019-04-20 22:08:31 +0530

    Removed not needed interceptors incase component unmounted

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-       <Layout>
-        <BurgerBuilder/>
-       </Layout>
-      </div>
-    );
-  }
+
+	render() {
+		return (
+			<div>
+				<Layout>
+					<BurgerBuilder/> 
+				</Layout>
+			</div>
+		);
+	}
 }
 
 export default App;
