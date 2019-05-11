a23de450288418a566c38ca77708b6f7e1da5e05
Nisarg <nisarg.gupta@moovweb.com>
2019-05-11 23:54:24 +0530

    Orderdata data passing to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -67,9 +67,14 @@ class ContactData extends Component {
 	orderHandler = (event) => {
 		event.preventDefault();
 		this.setState({loading: true});
+		const formData = {};
+		for (let formElement in this.state.orderForm) {
+			formData[formElement] = this.state.orderForm[formElement].value;
+		}
 		const order = {
 			ingredients: this.props.ingredients,
 			price: this.props.price,
+			orderData: formData
 		};
 		axios.post('/orders.json', order)
 			 .then( response => {
@@ -107,7 +112,7 @@ class ContactData extends Component {
 			});
 		}
 
-		let form = (<form>
+		let form = (<form onSubmit={this.orderHandler}>
 						{formElementsArray.map( formElement => (
 							<Input
 								key = {formElement.id} 
