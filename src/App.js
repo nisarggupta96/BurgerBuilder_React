abd014d7e560dd87f689689f542c11ad1fcb95bb
Nisarg <nisarg.gupta@moovweb.com>
2019-08-18 12:20:10 +0530

    React.lazy replaces asyncComponent

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
-import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
 	return import('./containers/Checkout/Checkout');
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
 	return import('./containers/Orders/Orders');
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
 	return import('./containers/Auth/Auth');
 });
 
@@ -29,7 +28,7 @@ const app = props => {
 	let routes = (
 		<Switch>
 			<Route path="/" exact component={BurgerBuilder} />
-			<Route path="/auth" component={asyncAuth} />
+			<Route path="/auth" render={() => <Auth />} />
 			<Redirect to="/" />
 		</Switch>
 	);
@@ -38,9 +37,9 @@ const app = props => {
 		routes = (
 			<Switch>
 				<Route path="/" exact component={BurgerBuilder} />
-				<Route path="/checkout" component={asyncCheckout} />
-				<Route path="/orders" component={asyncOrders} />
-				<Route path="/auth" component={asyncAuth} />
+				<Route path="/checkout" render={() => <Checkout />} />
+				<Route path="/orders" render={() => <Orders />} />
+				<Route path="/auth" render={() => <Auth />} />
 				<Route path="/logout" component={Logout} />
 				<Redirect to="/" />
 			</Switch>
@@ -49,7 +48,9 @@ const app = props => {
 	return (
 		<div>
 			<Layout>
-				{routes}
+				<Suspense fallback={<p>Loading...</p>}>
+					{routes}
+				</Suspense>
 			</Layout>
 		</div>
 	);
