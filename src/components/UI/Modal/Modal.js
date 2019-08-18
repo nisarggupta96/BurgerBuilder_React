167234a4aa8a09d3aa326847215732bcbf83b01a
Nisarg <nisarg.gupta@moovweb.com>
2019-08-18 13:56:36 +0530

    Modal, BurgerIngredient func component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -4,10 +4,10 @@ import Backdrop from "../Backdrop/Backdrop";
 
 import './Modal.css';
 
-const  modal = (props) => (
-	<Aux>
-		<Backdrop show={props.show} clicked={props.modalClosed}/>
-		<div 
+const modal = (props) => {
+	return (<Aux>
+		<Backdrop show={props.show} clicked={props.modalClosed} />
+		<div
 			className='Modal'
 			style={{
 				transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
@@ -16,6 +16,12 @@ const  modal = (props) => (
 			{props.children}
 		</div>
 	</Aux>
-);
+	);
+};
 
-export default modal;
+export default React.memo(
+	modal,
+	(prevProps, nextProps) =>
+		nextProps.show === prevProps.show &&
+		nextProps.children === prevProps.children
+);
